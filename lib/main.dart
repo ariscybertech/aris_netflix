0d86b3de2ee3343862f5036c756d65b9fdb1a1f8
ariscybertech <ariscybertech@gmail.com>
2021-12-24 10:33:01 +0500

    netflix

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/aris/pages/root_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RootPage(),
+    )
+  );
+}
